d45512e62a3250563426b6ba79e8072b23ad9612
I-am-Som <somanathmaharanaofficial@gmail.com>
2025-02-26 17:42:02 +0530

    ...

--- steganoGrpahy/src/main/java/com/Internship/steganography/SteganographyApplication.java
+++ steganoGrpahy/src/main/java/com/Internship/steganography/SteganographyApplication.java
@@ -1,5 +1,6 @@
 package com.Internship.steganography;
 
+import org.opencv.core.Core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,17 +9,15 @@ public class SteganographyApplication {
 
 	static {
 		try {
-			String opencvPath = System.getProperty("user.dir") + "/src/main/resources/libs/opencv_java490.dll";
-			System.load(opencvPath);
-			System.out.println("✅ OpenCV loaded successfully from: " + opencvPath);
+			// Load OpenCV native library automatically
+			System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+			System.out.println("✅ OpenCV Loaded Successfully");
 		} catch (UnsatisfiedLinkError e) {
 			System.err.println("❌ Failed to load OpenCV: " + e.getMessage());
 		}
 	}
 
-
 	public static void main(String[] args) {
 		SpringApplication.run(SteganographyApplication.class, args);
 	}
-
 }
